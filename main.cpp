952482699d704beeb59e6f55b5dbda1ba9a846e5
Harliamonti <123170910+Harliamonti@users.noreply.github.com>
2023-02-13 09:23:55 +0200

    Add examples of templates and type usage

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <typeinfo>
+#include <cmath>
+
+template <typename T, typename T2>
+auto add(T lhs, T2 rhs)
+{
+    return lhs + rhs;
+}
+
+template <typename T>
+auto foo(T number)
+{
+    if (std::trunc(number) == number)
+        return static_cast<int> (number);
+}
 
 int main()
 {
-    std::cout << "Hi Liam" << std::endl;
+    int a{ 5 };
+    double b{ 5.5 };
+    auto c = add(b, b);
+
+    std::cout << c << std::endl;
+    std::cout << typeid(c).name() << std::endl;
+
+    auto d{ foo(c)} ;
+
+    std::cout << d << std::endl;
+    std::cout << typeid(d).name() << std::endl;
+
 }
